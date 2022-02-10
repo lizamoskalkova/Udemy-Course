626f8ee1b0bb6f8be1136bd2277d66b0f57cbc77
lizamoskalkova <moskalkovalza@gmail.com>
2022-02-10 21:05:39 +0300

    Initial Commit

---
+++ LizaCard/LizaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LizaCardApp.swift
+//  LizaCard
+//
+//  Created by Elizaveta Moskalkova on 10.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct LizaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
